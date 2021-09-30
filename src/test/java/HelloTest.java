85e8920b5c5f12e312ea96c99e8235f1c747d636
Sarun Matipano <sarun.m@cdg.co.th>
2021-09-30 10:54:50 +0700

    [Update] test

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -3,17 +3,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class HelloTest {
-	
+
 	@Test
-    public void case01(){
-        // Arrange
-        Hello hello = new Hello();
-        String expected = "Hello Sarun";
-        
-        // Act
-        String actual = hello.say("Sarun");
-        
-        // Assert = Then
-        assertEquals(expected, actual);    }
+	public void case01() {
+		// Arrange
+		Hello hello = new Hello();
+		String expected = "Hello Sarun";
+
+		// Act
+		String actual = hello.say("Sarun");
+
+		// Assert = Then
+		assertEquals(expected, actual);
+	}
 
 }
